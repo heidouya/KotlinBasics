9373a2b179f62636263782ba7414217310e69c3d
heidou <294925572@qq.com>
2026-05-17 17:31:56 +0800

    feat: let作用域函数

--- app/src/main/java/com/example/kotlinbasics/8.Functions.kt
+++ app/src/main/java/com/example/kotlinbasics/8.Functions.kt
@@ -192,24 +192,74 @@ fun sum(x: Int, y: Int): Int {
 面向扩展的设计是 Kotlin 中的一种设计思想，允许你为一个已经存在的类添加新的功能，而不需要继承该类或使用设计模式。
 例如：
 */
-class HttpClient {
-    fun request(method: String, url: String, headers: Map<String, String>):String {
-        println("Requesting $method to $url with headers: $headers")
-        return "Response from $url"
-    }
-}
+// class HttpClient {
+//     fun request(method: String, url: String, headers: Map<String, String>):String {
+//         println("Requesting $method to $url with headers: $headers")
+//         return "Response from $url"
+//     }
+// }
+//
+// fun HttpClient.get(url: String) = request("GET", url, emptyMap())
+//
+// fun main() {
+//     val client = HttpClient()
+//
+//     val getResponseWithMember = client.request("GET", "https://example.com", emptyMap())
+//     println("请求结果1：$getResponseWithMember")
+//
+//     val getResponseWithExtension = client.get("https://example.com")
+//     println("请求结果2：$getResponseWithExtension")
+// }
+// -----------------作用域函数---------------------
+/*
+作用域函数是 Kotlin 中的一种功能，允许你在一个函数中访问另一个函数的成员。
+Kotlin 中有五种作用域函数：
+1. apply
+2. run
+3. with
+4. let
+5. also
 
-fun HttpClient.get(url: String) = request("GET", url, emptyMap())
+全局作用域（Global Scope）是指在整个程序中都可以访问的作用域。全局作用域中的函数和属性可以通过它们的名称直接访问，而不需要使用任何前缀。全局作用域是 Kotlin 中最外层的作用域，它是所有其他作用域的父作用域。
 
-fun main() {
-    val client = HttpClient()
+局部作用域（Local Scope）是指在某个函数或代码块中可以访问的作用域。它包含该函数或代码块中定义的函数和属性。例如：函数参数、局部变量和局部属性，它们只能在该函数或代码块中被调用。
+*/
+//-----------------------let------------------------------
+/*
+let 函数是 Kotlin 中的一种作用域函数，它接受一个 lambda 表达式作为参数，并将调用该函数的对象作为参数传递给 lambda 表达式。let 函数的返回值是 lambda 表达式的返回值。
 
-    val getResponseWithMember = client.request("GET", "https://example.com", emptyMap())
-    println("请求结果1：$getResponseWithMember")
+当你想在代码中执行空检查，之后再对返回的对象执行进一步操作时，请使用 let 作用域函数。
+*/
+// fun main() {
+//     // 报错
+//     // val address: String? = getNextAddress()
+//     // sendNotification(address)
+//
+//     //--------------使用条件判断-----------------
+//     // val address: String? = getNextAddress()
+//     // val confirm = if(address != null) {
+//     //     sendNotification(address)
+//     // } else { null }
+//     // println(confirm)
+//
+//     //--------------使用 let 函数-----------------
+//     val address: String? = getNextAddress()
+//     val confirm = address?.let {
+//         sendNotification(it)
+//     }
+//
+//     println(confirm)
+// }
+//
+// fun sendNotification(recipientAddress: String): String {
+//     println("Yo $recipientAddress!")
+//     return "Notification sent!"
+// }
+//
+// fun getNextAddress(): String {
+//     return "sebastian@jetbrains.com"
+// }
 
-    val getResponseWithExtension = client.get("https://example.com")
-    println("请求结果2：$getResponseWithExtension")
-}
 
 
 
