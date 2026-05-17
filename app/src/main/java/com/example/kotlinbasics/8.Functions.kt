5f8b81ee66edd44729d18eaa90bfdd74e556f77b
heidou <294925572@qq.com>
2026-05-17 16:22:42 +0800

    feat: 初识函数

---
+++ app/src/main/java/com/example/kotlinbasics/8.Functions.kt
@@ -0,0 +1,22 @@
+package com.example.kotlinbasics
+
+/*
+函数
+函数用于执行特定任务并可能返回结果。函数可以接受参数并可以有返回值。函数可以是命名的，也可以是匿名的。
+
+声明函数使用 fun 关键字，后跟函数名、参数列表和返回类型，函数的参数使用括号括起来，参数之间用逗号分隔如：
+fun sum(x: Int, y: Int): Int {
+    return x + y
+}
+*/
+fun main() {
+   val res = sum(1, 2)
+   println(res)
+}
+
+// 函数如果有返回值，返回值类型不能省略
+fun sum(x: Int, y: Int): Int {
+   return x + y
+}
+
+
