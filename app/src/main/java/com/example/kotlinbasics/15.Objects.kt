dfd32a161ea95543c7585f2e191f8ddfe8ceace7
heidou <294925572@qq.com>
2026-05-18 01:26:45 +0800

    feat: 对象声明

---
+++ app/src/main/java/com/example/kotlinbasics/15.Objects.kt
@@ -0,0 +1,28 @@
+package com.example.kotlinbasics
+
+/*
+对象（Object）：单例模式的实现，Kotlin中对象的定义更加简洁明了，无需使用 companion object
+*/
+//-----------------------对象声明-----------------------
+/*
+对象声明：使用 object 关键字声明的对象是单例的，即在整个程序运行期间，该对象只有一个实例存在。
+对象声明的语法如下：
+object 对象名 {
+    // 成员
+}
+对象声明的成员可以是属性、方法、构造函数等。
+*/
+fun main() {
+    println(Singleton.name)
+    Singleton.sayHello()
+}
+
+object Singleton {
+    var name: String = "Singleton"
+    fun sayHello() {
+        println("Hello, $name")
+    }
+}
+
+
+
