2f1ab69c17f05e342832541edf3c8f0ec7b04e06
heidou <294925572@qq.com>
2026-05-18 16:40:52 +0800

    feat: 成员函数

--- app/src/main/java/com/example/kotlinbasics/9.Classes.kt
+++ app/src/main/java/com/example/kotlinbasics/9.Classes.kt
@@ -9,31 +9,45 @@ class ClassName {
 数据类 自动实现 toString() equals() hashCode() copy()等函数，无需手动编写，简化代码，提高可读性，且可以比较对象是否相等，使用data关键字定义
 */
 //---------类属性------------
+// fun main() {
+//     // 在类名后的括号()内定义声明属性
+//     class Contact1(val id: Int, var email: String)
+//     // 创建实例
+//     val contact1 = Contact1(1, "mary@gmail.com")
+//     // 访问属性
+//     println(contact1.email)
+//
+//     //-----------------------------------
+//     // 在括号内声明不带 val 或 var 的属性，这些属性在实例创建后无法被访问
+//     class Contact2(id: Int, email:String)
+//     val contact2 = Contact2(1, "mary@gmail.com")
+//     // println(contact2.email)
+//
+//     //-------------类体中定义声明属性------------
+//     class Contact3(val id: Int, var email: String) {
+//         val category: String = ""
+//     }
+//     val contact3 = Contact3(1, "mary@gmail.com")
+//     println(contact3.email)
+//
+//     //--------------带默认值的构造函数----------
+//     class Contact4(val id: Int, var email: String = "example@gmail.com") {
+//         val category: String = "work"
+//     }
+//     val contact4 = Contact4(1)
+//     println(contact4.email)
+// }
+//--------------成员函数---------------
 fun main() {
-    // 在类名后的括号()内定义声明属性
-    class Contact1(val id: Int, var email: String)
-    // 创建实例
-    val contact1 = Contact1(1, "mary@gmail.com")
-    // 访问属性
-    println(contact1.email)
-
-    //-----------------------------------
-    // 在括号内声明不带 val 或 var 的属性，这些属性在实例创建后无法被访问
-    class Contact2(id: Int, email:String)
-    val contact2 = Contact2(1, "mary@gmail.com")
-    // println(contact2.email)
-
-    //-------------类体中定义声明属性------------
-    class Contact3(val id: Int, var email: String) {
-        val category: String = ""
+    class Contact(val id: Int, var email: String) {
+        // 成员函数
+        fun printId() {
+            println(id)
+        }
     }
-    val contact3 = Contact3(1, "mary@gmail.com")
-    println(contact3.email)
 
-    //--------------带默认值的构造函数----------
-    class Contact4(val id: Int, var email: String = "example@gmail.com") {
-        val category: String = "work"
-    }
-    val contact4 = Contact4(1)
-    println(contact4.email)
+    val contact = Contact(1, "mary@gmail.com")
+
+   contact.printId() // 1
 }
+
