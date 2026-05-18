82ee8a5e1f37cb0b99159a6b85ed66768b424f31
heidou <294925572@qq.com>
2026-05-18 14:20:02 +0800

    feat: 数组

--- app/src/main/java/com/example/kotlinbasics/6.Collections.kt
+++ app/src/main/java/com/example/kotlinbasics/6.Collections.kt
@@ -23,42 +23,62 @@ package com.example.kotlinbasics
 */
 
 //-----------------------List-----------------------
+// fun main() {
+//     // Read only list
+//     // 可以存放不同数据类型
+//     val readOnlyShapes = listOf("triangle", "square", "circle", 1,1, false)
+//     println(readOnlyShapes)
+//
+//     // 使用索引访问
+//     val res = readOnlyShapes[0]
+//     println("The first item in the list is: $res")
+//
+//     // 获取第一个元素
+//     val firstItem = readOnlyShapes.first()
+//     // 获取最后一个元素
+//     val lastItem = readOnlyShapes.last()
+//     // 集合中元素数量 count() 支持条件过滤
+//     val count = readOnlyShapes.count{it == 1}
+//     val size = readOnlyShapes.size
+//     // 某个元素是否存在集合中
+//     val isExist = "circle" in readOnlyShapes
+//
+//     println("$firstItem $lastItem count=$count $isExist")
+//
+//     // Mutable list with explicit type declaration
+//     val shapes: MutableList<String> = mutableListOf("triangle", "square", "circle")
+//
+//     // 添加元素
+//     shapes.add("pentagon")
+//     // 移除指定元素
+//     shapes.remove("pentagon")
+//     // 修改元素
+//     shapes[0] = "aaa"
+//
+//     println(shapes)
+//     println(shapes.size)
+//     println(shapes.count())
+//     println(shapes.isEmpty())
+//     println(shapes.isNotEmpty())
+// }
+
+//-----------------------Array-----------------------
+// kotlin中的数组：固定不变，创建后不能增删
 fun main() {
-    // Read only list
-    // 可以存放不同数据类型
-    val readOnlyShapes = listOf("triangle", "square", "circle", 1,1, false)
-    println(readOnlyShapes)
+    val arr: Array<Int> = arrayOf(1, 2, 3, 4, 5)
 
-    // 使用索引访问
-    val res = readOnlyShapes[0]
-    println("The first item in the list is: $res")
-
-    // 获取第一个元素
-    val firstItem = readOnlyShapes.first()
-    // 获取最后一个元素
-    val lastItem = readOnlyShapes.last()
-    // 集合中元素数量 count() 支持条件过滤
-    val count = readOnlyShapes.count{it == 1}
-    val size = readOnlyShapes.size
-    // 某个元素是否存在集合中
-    val isExist = "circle" in readOnlyShapes
-
-    println("$firstItem $lastItem count=$count $isExist")
-
-    // Mutable list with explicit type declaration
-    val shapes: MutableList<String> = mutableListOf("triangle", "square", "circle")
-
-    // 添加元素
-    shapes.add("pentagon")
-    // 移除指定元素
-    shapes.remove("pentagon")
     // 修改元素
-    shapes[0] = "aaa"
+    arr[0] = 0
 
-    println(shapes)
-    println(shapes.size)
-    println(shapes.count())
-    println(shapes.isEmpty())
-    println(shapes.isNotEmpty())
+   //  报错，没有add方法
+   // arr.add()
+   // 报错 Index 5 out of bounds for length 5
+   // arr[5] = 6;
 
+    println(arr.size) // 输出数组的大小
+
+    // 遍历数组
+    for (i in arr) println(i)
+    // 哈哈哈
+    print("Array elements: ")
 }
