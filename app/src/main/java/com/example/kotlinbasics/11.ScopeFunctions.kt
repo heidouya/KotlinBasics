b8292d6133c4c231179f83ee9cf18f73ba904475
heidou <294925572@qq.com>
2026-05-21 03:52:13 +0800

    feat: 作用域函数apply

--- app/src/main/java/com/example/kotlinbasics/11.ScopeFunctions.kt
+++ app/src/main/java/com/example/kotlinbasics/11.ScopeFunctions.kt
@@ -20,32 +20,54 @@ let 函数是 Kotlin 中的一种作用域函数，它接受一个 lambda 表达
 
 当你想在代码中执行空检查，之后再对返回的对象执行进一步操作时，请使用 let 作用域函数。
 */
+// fun main() {
+//     // 报错
+//     // val address: String? = getNextAddress()
+//     // sendNotification(address)
+//
+//     //--------------使用条件判断-----------------
+//     // val address: String? = getNextAddress()
+//     // val confirm = if(address != null) {
+//     //     sendNotification(address)
+//     // } else { null }
+//     // println(confirm)
+//
+//     //--------------使用 let 函数-----------------
+//     val address: String? = getNextAddress()
+//     val confirm = address?.let {
+//         sendNotification(it)
+//     }
+//
+//     println(confirm)
+// }
+//
+// fun sendNotification(recipientAddress: String): String {
+//     println("Yo $recipientAddress!")
+//     return "Notification sent!"
+// }
+//
+// fun getNextAddress(): String {
+//     return "sebastian@jetbrains.com"
+// }
+//--------------------apply--------------------
+/*
+apply 函数是 Kotlin 中的一种作用域函数，它接受一个 lambda 表达式作为参数，并将调用该函数的对象作为参数传递给 lambda 表达式。通常用于在代码中执行一些初始化操作，例如设置对象属性、调用对象方法等。apply 函数的返回值是调用该函数的对象，因此可以方便地链式调用。
+*/
+// val client = Client()
+
+val client = Client().apply {
+    token = "asdf"
+}
+
 fun main() {
-    // 报错
-    // val address: String? = getNextAddress()
-    // sendNotification(address)
+    client.token = "asdf"
+    client.getData()
+}
 
-    //--------------使用条件判断-----------------
-    // val address: String? = getNextAddress()
-    // val confirm = if(address != null) {
-    //     sendNotification(address)
-    // } else { null }
-    // println(confirm)
-
-    //--------------使用 let 函数-----------------
-    val address: String? = getNextAddress()
-    val confirm = address?.let {
-        sendNotification(it)
+class Client() {
+    var token: String? = null
+    fun getData() : String {
+        println("getting data!")
+        return "Mock data"
     }
-
-    println(confirm)
-}
-
-fun sendNotification(recipientAddress: String): String {
-    println("Yo $recipientAddress!")
-    return "Notification sent!"
-}
-
-fun getNextAddress(): String {
-    return "sebastian@jetbrains.com"
 }
